fc2e3b9f38629e6dccc04cd74ffa3c30183cfff9
MikeOHandy <mikehandy00@gmail.com>
2017-07-18 21:33:36 -0400

    Add files via upload

--- lottery.rb
+++ lottery.rb
@@ -1,56 +1,51 @@
-# def lotto(my_num, winning_num)
-# 	match = []
-# 	win_num.each do |win_num|
-# 		if win_num == my_num
-# 			match << my_num
-# 		end
-# 	end
-# 	match
-# end
-
-# def off_by_one(my_num, win_num)
-# 	count = 0
-# 		if my_num[0] == win_num[0]
-# 			count += 1
-# 		else 0
-# 		end
-# 		if my_num[1] == win_nums[1]
-# 			count +=1
-# 		else 0
-# 		end
-# 		if my_num[2] == win_nums[2]
-# 			count +=1
-# 		else 0
-# 		end
-# 		if my_num[3] == win_nums[3]
-# 			count +=1
-# 		else 0
-# 		end
-# 		if count == 3
-# 			true
-# 		end
-# 	end
-# def almost(my_num, win_num)
-# 	winner = []
-# 	win_num.each do |x|
-# 		if off_by_one(my_num, x) == true
-# 			winner << x
-# 		end
-# 	end
-# 	winner
-# end
-
-def count_matching_chars(str1,str2)
-  str1.each_char.count do |char| 
-    str2.include?(char)
+def lottery(my_num, win_num1)
+  match = []
+  win_num1.each do |match_num|
+    if match_num == my_num
+      match << my_num
+    end
   end
+  match
 end
 
-def matches(my_num, arr)
-  arr.select do |num|
-    return true if count_matching_chars(num,my_num) >= 3
+def off_by_one(my_num, win_num1)
+  count = 0
+  z=0
+   win_num1.length.times do
+     i=0
+    win_num1.length.times do
+      if my_num[z] == win_num1[i]
+        count += 1
+      end
+      i+=1
   end
+  z+=1
 end
+  count == win_num1.length - 1
+end
+p off_by_one("1234","1235")
+
+def almost_there(my_num, win_num1)
+  winning = []
+  win_num1.each do |y|
+    if off_by_one(my_num, y) == true
+      winning << y
+    end
+  end
+  winning
+end
+
+# def count_matching_chars(str1,str2)
+#   str1.each_char.count do |char| 
+#     str2.include?(char)
+#   end
+# end
+
+# def matches(my_num, arr)
+#   arr.select do |num|
+#     return true if count_matching_chars(num,my_num) >= 3
+#   end
+# end
 
 
 
--- lottery_test.rb
+++ lottery_test.rb
@@ -1,30 +1,39 @@
 require "minitest/autorun"
 require_relative "lottery.rb"
 
-class TestLottery < Minitest::Test
-	def test_blank
-	my_num = "6875"
-	win_num = []
-	assert_equal([], lotto(my_num, win_num))
-	end
-	def test_winning_num_match
-	my_num = "6875"
-	win_num = ["6875", "6523", "7876", "4543", "9176"]
-	assert_equal(["6875"], lotto(my_num, win_num))
-	end
-	def test_close_match
-	my_num = "6875"
-	win_num = "6865"
-	assert_equal(true, off_by_one(my_num, win_num))
-	end
-	def test_close_no_cigar
-	my_num = "6875"
-	win_num = ["5875", "6523", "7876", "4543", "9176"]
-	assert_equal(["5875"], almost(my_num, win_num))
-	end
-	def test_close_no_cigar2
-	my_num = "6875"
-	win_num = ["5875", "6523", "7875", "4543", "9176"]
-	assert_equal(["5875", "7875"], almost(my_num, win_num))
-	end
-end
+class TestCloseNoCigar < Minitest::Test
+  def test_nothing
+    my_num = "1234"
+    win_num = []
+    assert_equal([], lottery(my_num, win_num))
+  end
+  def test_winning_num_match
+    my_num = "1234"
+    win_num = ["1234", "4567", "8901", "2345", "6789"]
+    assert_equal(["1234"], lottery(my_num, win_num))
+  end
+  def test_close_match
+    my_num = "1234"
+    win_num = ["1244"]
+    assert_equal(true, off_by_one(my_num, win_num))
+  end
+  def test_close_no_cigar
+    my_num = "1234"
+    win_num = ["1235", "4567", "8901", "2345", "6789", "9234", "9123", "3412"]
+    assert_equal(["1235", "2345","9234","9123"],almost_there(my_num, win_num))
+  end
+ end
+
+
+
+# require"minitest/autorun"
+# require_relative "lottery.rb"
+# class Testlotto < Minitest::Test
+
+
+# def test_winning_number_match
+# 	my_num = (["6264"])
+# 	win_num = ["3334","3345","3363","3247","3266"]
+# 	assert_equal(["6264"],my_num)
+# end
+# end
